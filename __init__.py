18b0255da8f1a8eb8d12678652460c0225fc023c
Shiyu Wang <sw3601@columbia.edu>
2025-07-30 01:31:14 -0400

    Add notebooks, scripts, and PDFs

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from .models import ManifoldTraversal, Landmark, TraversalNetwork, TrainingResults
+from .utils import TISVD
+
+__version__ = "1.0.0"
+__all__ = ['ManifoldTraversal', 'Landmark', 'TraversalNetwork', 'TrainingResults', 'TISVD']
